19ca8200205ef15e3ef3c154a054724f39ce9ad5
Fredrik von Post <fredrik.vonpost@maxar.com>
2024-06-26 11:48:12 +0200

    typo in Name

--- Recipes/Ingredients/Ingredient.cs
+++ Recipes/Ingredients/Ingredient.cs
@@ -56,6 +56,6 @@ public class Cinnamon : Spice
 public class CocoaPowder : Ingredient
 {
     public override int Id => 8;
-    public override string Name => "CocoaPowder";
+    public override string Name => "Cocoa powder";
     public override string PreparationInstructions => $"{base.PreparationInstructions}";
 }
